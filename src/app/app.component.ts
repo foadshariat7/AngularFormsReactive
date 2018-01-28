6237f826d51a22820951f8802ae8381b38420f24
Foad R ShariatPanahi <foadshariat@outlook.com>
2018-01-28 01:45:20 -0800

    Foad - Completed video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators , FormArray} from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,57 @@ export class AppComponent implements OnInit {
   
   genders=['male', 'female'];
   inputForm: FormGroup;
-  
+  bannedEmails = ['foad@yahoo.com', 'foad@gmail.com'];
+
   ngOnInit(): void {
     this.inputForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('male')
+      'info': new FormGroup({
+        'username': new FormControl(null, Validators.required, this.bannedUsername),
+        'email': new FormControl(null, [Validators.required, Validators.email, this.bannedEmailCheck.bind(this)])
+      }),
+      'gender': new FormControl('male'),
+      'colors': new FormArray([])
+    });
+    // this.inputForm.valueChanges.subscribe(
+    //   (value) => {console.log(value)}
+    // );
+    this.inputForm.statusChanges.subscribe(
+      (status) => {console.log(status)}
+    );
+    this.inputForm.patchValue({
+      'info': {
+        'username': 'ugg'
+      }
     });
   }
 
   onSubmit() {
     console.log(this.inputForm);
+  }
+
+  onAddColorsClick(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.inputForm.get('colors')).push(control);
+  }
+
+  bannedEmailCheck(control: FormControl): {[e: string]: boolean}{
+    if(this.bannedEmails.indexOf(control.value) !== -1){
+      return {'isBannedEmail': true};
+    }
+    return null;
+  }
+
+  bannedUsername(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === "foad"){
+          resolve({'usernameIsBanned': true});
+        } else{
+          resolve(null);
+        }
+      },2000);
+    });
+    return promise;
   }
 
 }
